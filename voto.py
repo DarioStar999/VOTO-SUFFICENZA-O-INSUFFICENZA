f5fcd01eaf1f50b58ec61f8b9f43a5127b5bedc0
DarioStar999 <105985378+DarioStar999@users.noreply.github.com>
2023-04-01 21:06:28 +0200

    Add files via upload

---
+++ voto.py
@@ -0,0 +1,9 @@
+voto = input("inserisci il tuo voto: ")
+if voto<="6":
+   print("Testa di cazzo hai preso un insufficenza")
+if voto>= "6":
+   print("bravo hai preso la sufficenza")
+if voto>= "8":
+   print("sei un cazzo di genio")
+input("made whit code by Dario Vaghi: ")
+exit()
