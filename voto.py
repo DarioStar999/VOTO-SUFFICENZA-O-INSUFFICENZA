b73803cc3ecc74ee7f82eb2b52150acf755f7c14
DarioStar999 <105985378+DarioStar999@users.noreply.github.com>
2023-04-01 21:08:07 +0200

    Update voto.py

--- voto.py
+++ voto.py
@@ -3,6 +3,8 @@ if voto<="6":
    print("Testa di cazzo hai preso un insufficenza")
 if voto>= "6":
    print("bravo hai preso la sufficenza")
+if voto>= "7":
+   print("Non male dai")
 if voto>= "8":
    print("sei un cazzo di genio")
 input("made whit code by Dario Vaghi: ")
